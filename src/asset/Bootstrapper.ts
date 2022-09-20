2c20fd90b0be2648a9d4a6bd6d6e99553858064c
noname <48761044+noname0310@users.noreply.github.com>
2022-09-21 08:59:15 +0900

    create project

---
+++ src/asset/Bootstrapper.ts
@@ -0,0 +1,9 @@
+import {
+    Bootstrapper as BaseBootstrapper, SceneBuilder
+} from "the-world-engine";
+
+export class Bootstrapper extends BaseBootstrapper {
+    public run(): SceneBuilder {
+        throw new Error("Method not implemented.");
+    }
+}
