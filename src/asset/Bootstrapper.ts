97c14ca0472d36cbbbfc6b42fdc10284ba04ae09
noname <48761044+noname0310@users.noreply.github.com>
2022-09-21 09:26:38 +0900

    build scene

--- src/asset/Bootstrapper.ts
+++ src/asset/Bootstrapper.ts
@@ -1,9 +1,33 @@
 import {
-    Bootstrapper as BaseBootstrapper, SceneBuilder
+    AsyncImageLoader,
+    Bootstrapper as BaseBootstrapper, 
+    Camera,
+    CssTilemapChunkRenderer,
+    SceneBuilder,
+    TileAtlasItem
 } from "the-world-engine";
+
+import SampleTiles from "./texture/sample_tiles.png";
 
 export class Bootstrapper extends BaseBootstrapper {
     public run(): SceneBuilder {
-        throw new Error("Method not implemented.");
+        const instantiater = this.instantiater;
+
+        return this.sceneBuilder
+            .withChild(instantiater.buildGameObject("camera")
+                .withComponent(Camera))
+
+            .withChild(instantiater.buildGameObject("map")
+                .withComponent(CssTilemapChunkRenderer, c => {
+                    c.chunkSize = 15;
+
+                    AsyncImageLoader.loadImageFromPath(SampleTiles).then(image => {
+                        if (!c.exists) return;
+                        
+                        c.imageSources = [ new TileAtlasItem(image, 13, 9) ];
+                        c.drawTile(0, 0, 0, 0);
+                    });
+                }))
+        ;
     }
 }
